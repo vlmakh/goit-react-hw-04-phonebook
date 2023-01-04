3914e1ca2989620144bd22f4a8fa6124b1c3c115
Vlad Makhnenko <vlmakh@gmail.com>
2023-01-04 17:46:00 +0200

    update proptypes ContactList

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -21,6 +21,12 @@ export function ContactList({ contacts, deleteContact }) {
 }
 
 ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ),
   deleteContact: PropTypes.func.isRequired,
 };
